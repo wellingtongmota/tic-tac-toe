8a964a60b1be1a3410af860640802f20b09aed5c
Wellington Galvao <wellingtongalvao96@gmail.com>
2022-07-03 16:05:05 -0300

    Adicionado funcao preencheMapa

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 const verificaPosicao = (posicao) => {
-    if (posicao == 'X' || posicao == 'O') {
-        console.log('já tem um valor')
+    if (posicao == 'X' || posicao == 'O')
         return false
-    }
+    
     return true
 }
 
+const preencheMapa = () => {
+    for (var pos = 0; pos < boxs.length; pos++){
+        mapa.push(boxs[pos].textContent)
+    }
+}
+
 const jogo = document.querySelector('.wrapper')
+var boxs = document.querySelectorAll('[data-action]')
 var jogada = 0
+var mapa = []
 
 jogo.addEventListener('click', e => {
     const div = e.target
     const action = div.dataset.action
     const posicao = div.textContent
 
+    if (mapa.length == 0)
+        preencheMapa()
+
+    console.log(mapa)
+
     if (jogada % 2 == 0) {
-        if(verificaPosicao(posicao)) {
+        if(verificaPosicao(posicao))
             div.textContent = 'X'
-            console.log('valor X aplicado')
-        } else {
-            console.log('já existe o valor X valor')
-        }
     } else {
-        if(verificaPosicao(posicao)) {
-            div.textContent = 'O'
-            console.log('valor O aplicado')
-        } else {
-            console.log('já existe o valor O valor')
-        }
+        if(verificaPosicao(posicao))
+            div.textContent = 'O' 
     }
+
     jogada++
 
     // console.log(action)
