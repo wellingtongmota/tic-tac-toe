2da285edf6924f6f7079c5610415b859f6bd53b2
Wellington Galvao <wellingtongalvao96@gmail.com>
2022-07-03 13:38:27 -0300

    Adicionando funcao verificaJogada

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
+const verificaPosicao = (player, jogada, posicao) => {
+    if (posicao == 'X' || posicao == 'O') {
+        console.log('já tem um valor')
+         return jogada--
+    } else {
+        return jogada++
+    }
+}
+
 const jogo = document.querySelector('.wrapper')
 var jogada = 0
 
 jogo.addEventListener('click', e => {
     const div = e.target
-    const action = div.dataset.action
+    const posicao = div.dataset.action
     const valor = div.textContent
 
     if (valor == 'X' || valor == 'O')
         console.log('já tem um valor')
     else {
-        if(jogada % 2 == 0) {
-            div.textContent = 'X'
-            jogada++
+        if (jogada % 2 == 0) {
+            jogada = verificaPosicao('X', jogada, posicao)
         } else {
-            div.textContent = 'O'
-            jogada++
+            jogada = verificaPosicao('O', jogada, posicao)
         }
     }
     console.log(action)
